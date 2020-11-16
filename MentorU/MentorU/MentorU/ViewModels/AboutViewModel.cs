ff6d34b3367dae610198509536355dd7182d40d1
Bamuir <bamuir3@outlook.com>
2020-11-15 22:51:55 -0700

    Messing with xamarin
    Looking at how things work, how refactoring works for the about -> homepage name, etc.

--- MentorU/MentorU/MentorU/ViewModels/AboutViewModel.cs
+++ MentorU/MentorU/MentorU/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace MentorU.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "Home";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 

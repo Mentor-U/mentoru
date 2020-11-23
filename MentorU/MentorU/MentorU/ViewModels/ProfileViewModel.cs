a49a1b6aca54d346f2554bed0d2537a99d2bed48
Jon Warner <u1003431@utah.edu>
2020-11-22 22:03:31 -0800

    Changed mentor list view in profile page to collectionview. Added LoadPageDataCommand to profileVM to load data from datastore for mentor list. FIXME: not fully async due to hard coded values instead of an async call to the datastore

--- MentorU/MentorU/MentorU/ViewModels/ProfileViewModel.cs
+++ MentorU/MentorU/MentorU/ViewModels/ProfileViewModel.cs
@@ -12,11 +12,13 @@ namespace MentorU.ViewModels
     {
         private User _user;
         public Command EditProfileCommand { get; }
+        public Command LoadPageDataCommand { get; }
 
         /* Attributes from the user that are needed for dispaly */
         public string Name { get => _user.Name; }
         public string Major { get => _user.Major; }
         public ObservableCollection<string> Classes { get; }
+        public ObservableCollection<User> Mentors { get; }
         public string Bio { get => _user.Bio; }
 
         /***
@@ -27,12 +29,38 @@ namespace MentorU.ViewModels
             // TODO: get user from data base as they should already exist if they are on this page
             _user = new User("Wallace");
             Title = "Profile";
+            Mentors = new ObservableCollection<User>();
+
+            //TODO: add all commands for loading market place recommendations and fetching user data from DB
+            LoadPageDataCommand = new Command(async () => await ExecuteLoadMentors()); 
             EditProfileCommand = new Command(EditProfile);
         }
 
         private async void EditProfile(object obj)
         {
             await Shell.Current.GoToAsync(nameof(EditProfilePage));
+        }
+
+        async Task ExecuteLoadMentors() //TODO: make async with the call to the datastore
+        {
+            IsBusy = true;
+            try
+            {
+                Mentors.Clear();
+                // var mentors = await DataStore.GetMentorsAsync(); // TODO: add datastore method
+                User m1 = new User("George");
+                User m2 = new User("Steve");
+                Mentors.Add(m1);
+                Mentors.Add(m2);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         public void OnAppearing()

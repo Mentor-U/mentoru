acc27a3f5348e698896000a36163575e279e1aed
Jon Warner <u1003431@utah.edu>
2020-11-23 20:07:09 -0800

    Added the views and viewmodel for view other peoples profiles. Currently not linked to anything

---
+++ MentorU/MentorU/MentorU/ViewModels/ViewOnlyProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MentorU.Models;
+using MentorU.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MentorU.ViewModels
+{
+
+    public class ViewOnlyProfileViewModel : BaseViewModel
+    {
+        private User _user;
+        public string Name { get => _user.Name; }
+        public string Field { get => _user.Major; }
+        public string Bio { get => _user.Bio; }
+
+        public ViewOnlyProfileViewModel()
+        {
+            _user = new User("George");
+            _user.Bio = "I enjoy good coffee and helping people with programming";
+        }
+
+    }
+}

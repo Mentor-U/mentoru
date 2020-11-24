b69f1600ed1de009b8c2335db0a1ac8e220c5e3b
Jon Warner <u1003431@utah.edu>
2020-11-23 21:52:49 -0800

    Added chat view and viewmodels

---
+++ MentorU/MentorU/MentorU/ViewModels/MainChatViewModel.cs
@@ -0,0 +1,51 @@
+﻿using MentorU.Models;
+using MentorU.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MentorU.ViewModels
+{
+    public class MainChatViewModel : BaseViewModel
+    {
+        private User _user;
+        public ObservableCollection<User> Chats { get; }
+        public Command LoadChatsCommand { get; }
+        public MainChatViewModel()
+        {
+            Title = "Chats";
+            _user = new User("Wallace");
+            Chats = new ObservableCollection<User>();
+            LoadChatsCommand = new Command(async () => await ExecuteLoadChats());
+        }
+
+        async Task ExecuteLoadChats()
+        {
+            IsBusy = true;
+            try
+            {
+                Chats.Clear();
+                // var chats = await Data.GetChatsAsync(true);
+                User u1 = new User("Bob");
+                User u2 = new User("Steve");
+                Chats.Add(u1);
+                Chats.Add(u2);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+        }
+    }
+}

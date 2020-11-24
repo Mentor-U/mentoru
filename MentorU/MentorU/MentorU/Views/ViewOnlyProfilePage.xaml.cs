92631c8b1f0111f7a538fef9c4ef63dec6010077
Jon Warner <u1003431@utah.edu>
2020-11-23 20:21:31 -0800

    Resolved naming issue in the ViewOnlyProfile.xaml.cs

--- MentorU/MentorU/MentorU/Views/ViewOnlyProfilePage.xaml.cs
+++ MentorU/MentorU/MentorU/Views/ViewOnlyProfilePage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MentorU.ViewModels;
+using MentorU.Views;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,7 +17,7 @@ namespace MentorU.Views
         public ViewOnlyProfilePage()
         {
             InitializeComponent();
-            BindingContext = new ViewOnlyProfilePage();
+            BindingContext = new ViewOnlyProfileViewModel();
         }
     }
 }

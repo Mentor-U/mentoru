f15ba2d12a60d5c594539f7a43bdd17644894eee
KHOI TRAN <minhkhoiii1997@gmail.com>
2020-12-02 01:37:42 -0700

    Re-create user table and implement partial user database

--- MentorU/MentorU/MentorU/Models/User.cs
+++ MentorU/MentorU/MentorU/Models/User.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MentorU.Models
 {
     public class User
     {
+        [PrimaryKey, AutoIncrement, Column("_Id")]
         public int UserID { get; set; }
-        
         public string UserName { get; set; }
         public string Name { get; set; }
         public string Major { get; set; }
@@ -18,12 +19,12 @@ namespace MentorU.Models
 
         public List<string> Classes;
 
-        public User(string name)
-        {
-            Name = name;
-            Major = "Computer Science";
-            Bio = "Beep boop bop this is me talking about myself!";
-            Classes = new List<string>();
-        }
+        //public User(string name)
+        //{
+        //    Name = name;
+        //    Major = "Computer Science";
+        //    Bio = "Beep boop bop this is me talking about myself!";
+        //    Classes = new List<string>();
+        //}
     }
 }

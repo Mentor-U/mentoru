22cc83fb3f1a4d5b2bdfbe9687a9cd1e2dd3ecdc
KHOI TRAN <minhkhoiii1997@gmail.com>
2020-12-04 12:12:31 -0700

    Create database class which create a User table

---
+++ MentorU/MentorU/MentorU/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using MentorU.Models;
+
+namespace MentorU
+{
+    class Database
+    {
+        readonly SQLiteAsyncConnection _context;
+
+        public Database(string dbPath)
+        {
+            _context = new SQLiteAsyncConnection(dbPath);
+            _context.CreateTableAsync<User>().Wait();
+        }
+
+        public Task<List<User>> GetUserAsync()
+        {
+            return _context.Table<User>().ToListAsync();
+        }
+
+        public Task<int> SaveUserAsync(User user)
+        {
+            return _context.InsertAsync(user);
+        }
+    }
+}

f0edd7c891449c95edd6902ae891a97135753b80
Jon Warner <jonathanstaynerwarner@gmail.com>
2020-12-16 11:24:53 -0700

    fixed chat UI to have multiple lines of text

--- MentorU/MentorU/MentorU/ViewModels/ChatViewModel.cs
+++ MentorU/MentorU/MentorU/ViewModels/ChatViewModel.cs
@@ -36,7 +36,7 @@ namespace MentorU.ViewModels
                 // TODO: load message history
                 List<Message> messages = new List<Message>(); //REMOVE ME: (placeholder)
                 messages.Add(new Message() { User = App.loggedUser, Mine = true, Theirs = false, Text = "Hello There" });
-                messages.Add(new Message { User = new Users { FirstName = "Bob"}, Mine = false, Theirs = true, Text = "What's up nerd?" });
+                messages.Add(new Message { User = new Users { FirstName = "Bob"}, Mine = false, Theirs = true, Text = "Hi, how are you today? What can I help you with? " });
                 foreach (var m in messages)
                 {
                     Messages.Add(m);

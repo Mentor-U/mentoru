dd05e26f1d68949195762d6344772b4eb22a9bca
u1189514 <u1189514@utah.edu>
2021-01-14 20:50:42 -0700

    Add connection model

---
+++ MentorU/MentorU/MentorU/Models/Connection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MentorU.Models
+{
+    public class Connection
+    {
+        public string MentorID { get; set; }
+        public string MenteeID { get; set; }
+    }
+}
---
+++ MentorU/MentorU/MentorU/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MentorU.Models;
+using MentorU.Views.ChatViews;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MentorU.ViewModels
+{
+    public class NotificationViewModel : BaseViewModel
+    {
+
+    }
+}

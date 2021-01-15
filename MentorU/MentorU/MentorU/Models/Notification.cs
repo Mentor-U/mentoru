2ccf17ea623296f30e23b1388c663e0c36b02053
u1189514 <u1189514@utah.edu>
2021-01-14 20:52:29 -0700

    add notification model

---
+++ MentorU/MentorU/MentorU/Models/Notification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MentorU.Models
+{
+    public class Notification
+    {
+        public string UserID { get; set; }
+        public string Message { get; set; }
+    }
+}

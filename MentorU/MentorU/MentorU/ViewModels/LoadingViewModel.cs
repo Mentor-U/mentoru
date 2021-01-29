898b6cf77cafabcdf525b25a0547c7e419998cf4
Brayden Muir (BAM) <bamuir3@outlook.com>
2021-01-28 17:03:47 -0700

    Auto Sign In
    No more need to click login if your credentials are already cached (you havent signed yourself out)

--- MentorU/MentorU/MentorU/ViewModels/LoadingViewModel.cs
+++ MentorU/MentorU/MentorU/ViewModels/LoadingViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using MentorU.Models;
+using MentorU.Services.DatabaseServices;
+using MentorU.Services.LogOn;
+using System;
+using Xamarin.Forms;
 
 namespace MentorU.ViewModels
 {
@@ -12,7 +16,43 @@ namespace MentorU.ViewModels
         // Called by the views OnAppearing method
         public async void Init()
         {
-            await Shell.Current.GoToAsync("///Login");
+            try
+            {
+                var userContext = await B2CAuthenticationService.Instance.SignInAsync();
+                App.AADUser = userContext;
+
+                Users tempUser = new Users
+                {
+                    id = userContext.UserIdentifier,
+                    FirstName = userContext.GivenName,
+                    LastName = userContext.FamilyName,
+                    DisplayName = userContext.Name,
+                    Email = userContext.EmailAddress,
+                    Role = "1",
+                    Major = "CS",
+                    Bio = "test"
+                };
+
+                App.loggedUser = tempUser;
+
+                bool isNew = await DatabaseService.Instance.tryCreateAccount(tempUser);
+
+                if (isNew)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Success", "Account Created", "Ok");
+                    // create profile here
+                }
+
+
+                await Shell.Current.GoToAsync("///Home");
+
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.GoToAsync("///Login");
+            }
+            //var userContext = await B2CAuthenticationService.Instance.SignInAsync();
+           
         }
     }
 }

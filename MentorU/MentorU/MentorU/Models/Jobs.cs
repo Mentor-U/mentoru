5b4d184a09365d9ad72462a49966cec686f4429f
KHOI TRAN <u1189514@utah.edu>
2021-03-25 21:09:47 -0600

    add Jobs model

---
+++ MentorU/MentorU/MentorU/Models/Jobs.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using Xamarin.Forms;
+
+namespace MentorU.Models
+{
+    /// <summary>
+    /// Represents a Job Listing
+    /// </summary>
+    public class Jobs
+    {
+        [PrimaryKey]
+        public string id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Owner { get; set; }
+
+        public string JobType { get; set; }
+        public string Level { get; set; }
+
+        [Ignore]
+        public ImageSource jobImage { get; set; }
+    }
+}
---
+++ MentorU/MentorU/MentorU/ViewModels/JobBoardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MentorU.Models;
+using MentorU.Services.Blob;
+using MentorU.Services.DatabaseServices;
+using MentorU.Views;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Linq;
+
+namespace MentorU.ViewModels
+{
+    public class JobBoardViewModel : BaseViewModel
+    {
+    }
+}

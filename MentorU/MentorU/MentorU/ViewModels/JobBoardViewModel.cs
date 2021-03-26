d5d7d70063f0c9b2f9c63ded446fcba123d2099e
KHOI TRAN <u1189514@utah.edu>
2021-03-25 21:27:17 -0600

    Implement JobBoard UI

--- MentorU/MentorU/MentorU/ViewModels/JobBoardViewModel.cs
+++ MentorU/MentorU/MentorU/ViewModels/JobBoardViewModel.cs
@@ -15,5 +15,30 @@ namespace MentorU.ViewModels
 {
     public class JobBoardViewModel : BaseViewModel
     {
+        private Jobs _selectedJob;
+        public ObservableCollection<string> AllLevel { get; set; }
+        private string _level;
+        private string _jobType;
+
+        public ObservableCollection<Jobs> Jobs { get; }
+        public Command LoadJobsCommand { get; }
+        public Command AddJobCommand { get; }
+        public Command<Jobs> JobTapped { get; }
+        public Command FilterCommand { get; }
+        public Command ClearFilters { get; }
+        public Command ClosePopUp { get; set; }
+
+        public ObservableCollection<string> Filters { get; }
+
+        private string _filters;
+        public string ShowFilters
+        {
+            get => _filters;
+            set
+            {
+                _filters = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }

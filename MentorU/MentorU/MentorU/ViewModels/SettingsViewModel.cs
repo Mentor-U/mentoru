7d332fdc2eb3cd531ef1c402e28644078fa967a5
Ajay Bagali <ajaybagali@gmail.com>
2021-04-04 17:28:38 -0600

    added settings storage to db

--- MentorU/MentorU/MentorU/ViewModels/SettingsViewModel.cs
+++ MentorU/MentorU/MentorU/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MentorU.Models;
+using MentorU.Services.DatabaseServices;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,6 +33,39 @@ namespace MentorU.ViewModels
 
         private async void OnSave()
         {
+           
+            var usersList = await DatabaseService.Instance.client.GetTable<Settings>().Where(u => u.UserID == App.loggedUser.id).ToListAsync();
+
+            if (usersList.Count == 0)
+            {
+
+                Settings newSettings = new Settings()
+                {
+                   UserID = App.loggedUser.id, 
+                   EmailSettings = _emailSwitch
+                };
+
+                await DatabaseService.Instance.client.GetTable<Settings>().InsertAsync(newSettings);
+            }
+            else
+            {
+                var settingsList = await DatabaseService.Instance.client.GetTable<Settings>().Where(u => u.UserID == App.loggedUser.id).ToListAsync();
+  
+
+                JObject data = new JObject
+                {
+                    {"id",  settingsList[0].id},
+                    {"UserID", App.loggedUser.id },
+                    {"EmailSettings", _emailSwitch },
+  
+                };
+                await DatabaseService.Instance.client.GetTable<Settings>().UpdateAsync(data);
+            }
+
+
+
+            await Application.Current.MainPage.DisplayAlert("Alert", "Settings have been changed.", "Ok");
+
 
         }
 
